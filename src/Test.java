d5661e460acfd37c3271bfd6fd2a181336a00347
Lilit Arzumanyan <02.lilit@gmail.com>
2021-03-30 22:03:15 +0400

    initial commit

---
+++ src/Test.java
@@ -0,0 +1,21 @@
+public class Test {
+    public static void main(String[] args) {
+        byte b = 50;
+        short s = 800;
+        int i = 9600;
+        long l = 56700000000L;
+        float f = 55.55555f;
+        double d = 89.999;
+        boolean bool = true;
+        char c = '%';
+
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(bool);
+        System.out.println(c);
+    }
+}
